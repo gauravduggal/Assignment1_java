30674e264e4097656b87e9f488b7679291b50624
gd <gd@gdx>
2017-08-23 17:06:32 +0530

    shifted file operations to heap class, added functionality to show heap, left, right and parent node. Need to implement heapify

--- assignment1.java
+++ assignment1.java
@@ -1,7 +1,4 @@
 import java.util.*;  
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class assignment1
 {
@@ -13,46 +10,15 @@ public class assignment1
    public static void main(String []args) {
       
       heap h = new heap();
-      String fileName = "/home/gd/IIITD/OOPD/codes/assignment1/data.txt";
-      try (Scanner scanner = new Scanner(new File(fileName))) {
-
-      while (scanner.hasNext()){
-        String line = scanner.nextLine();
-        //System.out.println(line);
-        String [] part = line.split(" ");
-        student s1 = new student();
-        switch(part[0])
-        {
-          case "INSERT":
-          s1.update_from_file(line);
-          //insert at the last node
-          h.insert_student(s1);
-          //heapify nlogn times
-          break;
-          case "MAXIMUM":
-          break;
-          case "EXTRACT-MAX":
-          break;
-          case "DELETE":
-          break;
-          case "SHOW":
-          break;
-                   
-          default:
-          System.out.println("error");
-          break;
-        }
-        
-       System.out.println(h.get_length_heap());
+      h.heap_file("/home/gd/IIITD/OOPD/codes/assignment1/data.txt");
+      
+      // System.out.println(h.get_right_child(4).first_name);
       
       }
 
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
+    
         
       //System.out.println(h.get_student(0).first_name);
       
-   }
+   
 }
--- heap.java
+++ heap.java
@@ -1,4 +1,8 @@
 import java.util.*;  //For adding ArrayList
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class heap
 {
   //Attributes
@@ -21,7 +25,88 @@ public class heap
    {
     return this.student_list.get(index);
    }
-   
+   void heapify()
+   {
+    int cur_index=length-1;
+    
+   }
+
+   student get_left_child(int i)
+   {
+    if ((2*i+1) < (this.length))
+      return this.student_list.get(2*i+1);
+    else
+      return new student();
+   }
+
+   student get_right_child(int i)
+   {
+    if ((2*i+2) < (this.length))
+      return this.student_list.get(2*i+2);
+    else
+      return new student();
+   }
+
+   student get_parent(int i)
+   {
+    // System.out.println((i-1)/2);
+    if (i%2==0 && ((i-2)/2>=0)&& i<length)
+      return this.student_list.get((i-2)/2);
+    else if(i%2==1 && ((i-1)/2>=0) && i<length)
+      return this.student_list.get((i-1)/2);  
+    else
+      return new student();
+   }
+
+   void heap_file(String fileName)
+   {
+      try (Scanner scanner = new Scanner(new File(fileName))) {
+
+      while (scanner.hasNext()){
+        String line = scanner.nextLine();
+        //System.out.println(line);
+        String [] part = line.split(" ");
+        student s1 = new student();
+        switch(part[0])
+        {
+          case "INSERT":
+          s1.update_from_file(line);
+          //insert at the last node
+          this.insert_student(s1);
+          //heapify nlogn times
+          break;
+          case "MAXIMUM":
+          break;
+          case "EXTRACT-MAX":
+          break;
+          case "DELETE":
+          break;
+          case "SHOW":
+          System.out.println("Current List");
+          for(int i=0;i<this.length;i++)
+          {
+            s1=this.student_list.get(i);
+            System.out.println((i)+". "+s1.first_name+" "+s1.last_name);
+          }
+          System.out.println();
+          break;
+                   
+          default:
+          System.out.println("error");
+          break;
+        }
+        
+        }
+      }
+
+        catch (IOException e)
+         {
+          e.printStackTrace();
+          }
+
+      
+   }
+
    int get_length_heap()
    {
     return length;
